fde2d21d324614f37274902296740b3df072b518
learnb <learn.bryan@gmail.com>
2013-04-27 05:22:54 -0400

    ugly jumping

--- LudumDare/src/net/umc/ludumdare/states/GamePlayState.java
+++ LudumDare/src/net/umc/ludumdare/states/GamePlayState.java
@@ -14,10 +14,24 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class GamePlayState extends BasicGameState{
 
+	//game variables
     int stateID = 1;
     private int screenWidth, screenHeight;
     private Level level;
     private boolean pause;
+    private Input input;
+    
+    //main character variables
+    private Sprite mainChar;
+    private boolean jumping = false;	//ascending
+    private boolean falling = false;	//descending
+    private int jumpTimerMax = 400;
+    private int fallTimerMax = 400;
+    private int jDelayTimerMax = 500;
+    private int jumpTimer = 0;
+    private int fallTimer = 0;
+    private int jDelayTimer = jDelayTimerMax;
+
     
     public GamePlayState( int stateID ) 
     {
@@ -45,18 +59,56 @@ public class GamePlayState extends BasicGameState{
     	if (pause) return;
     	
     	level.update(gc, sbg, delta);
-    	Input input = gc.getInput();
-    	Sprite mainChar = level.getMainChar();
+    	input = gc.getInput();
+    	mainChar = level.getMainChar();
+
+    	
+    	System.out.println("\n\njump: "+jumping+"\nfall:"+falling);
+    	if(!jumping && !falling){
+    		jDelayTimer+=delta;
+    	}
+    	if(jumping){	//increment jumpTimer
+    		jumpTimer+=delta;
+    	}
+    	if(falling){	//increment fallTimer
+    		fallTimer+=delta;
+    	}
+    	if(fallTimer>=fallTimerMax){
+    		//stop falling
+    		falling = false;
+    		fallTimer=0;
+    		jDelayTimer = 0;
+    	}
+    	
+    	//main character controls
     	if (input.isKeyDown(Input.KEY_LEFT)) {
     		if (mainChar.getX() - 4 >= 0) {
     			mainChar.setX(mainChar.getX() - 4);
     		}
     	}
-    	else if (input.isKeyDown(Input.KEY_RIGHT)) {
+    	if (input.isKeyDown(Input.KEY_RIGHT)) {
     		if (mainChar.getX() + 64 + 4 <= 448) {
     			mainChar.setX(mainChar.getX() + 4);
     		}
     	}
+    	if ((!input.isKeyDown(Input.KEY_SPACE) && jumping) || jumpTimer>=jumpTimerMax) {
+    		//start falling
+    		System.out.println("FALLING!");
+    		falling = true;
+    		jumpTimer = 0;
+    		fallTimer = 0;
+    		jumping = false;
+    	}
+    	if (input.isKeyDown(Input.KEY_SPACE) && !falling && (jDelayTimer>=jDelayTimerMax) && !(jumpTimer>=jumpTimerMax)) {
+    		//start jumping
+    		System.out.println("JUMPING!");
+    		if(!jumping){
+    			jumpTimer = 0;
+    		}
+    		jumping = true;
+    		
+    	}
+    	
     	
     	if (mainChar.getY() <= 0) {
     		return;
@@ -65,7 +117,7 @@ public class GamePlayState extends BasicGameState{
     	int panelX = (int)(mainChar.getX() + 32) / 64;
     	int panelY = (int)(mainChar.getY() + 48) / 64;
     	
-    	if (!level.getPlatform()[panelX][panelY]) {
+    	if ((!jumping && !falling) && !level.getPlatform()[panelX][panelY]) {
     		//pause = true;
     		level.init();
     	}
